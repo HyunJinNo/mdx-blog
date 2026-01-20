40372215073ee064677d0a0132d41798087cb3b7
HyunJinNo <nhj9831@naver.com>
2026-01-20 17:13:58 +0900

    feat: NavItem 컴포넌트 구현

---
+++ src/shared/ui/navItem/NavItem.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItemProps {
+  title: string;
+  href: string;
+  children?: React.ReactNode;
+}
+
+export const NavItem = ({ title, href, children }: NavItemProps) => {
+  const pathname = usePathname();
+
+  return (
+    <li>
+      <Link
+        className={[
+          pathname === href && "bg-white/50",
+          "flex h-10 flex-row items-center gap-6 rounded-xl px-4 duration-300 hover:bg-white/50",
+        ].join(" ")}
+        href={href}
+      >
+        {children}
+        <span className="text-sm font-semibold">{title}</span>
+      </Link>
+    </li>
+  );
+};
