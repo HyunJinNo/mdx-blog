ea75f825bfe4c68981eb72a3e128a8554043c7f4
HyunJinNo <nhj9831@naver.com>
2026-01-23 16:21:10 +0900

    design: TrendingTag 컴포넌트 UI 구현

---
+++ src/shared/ui/tag/TrendingTag.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+interface TrendingTagProps {
+  title: string;
+}
+
+export const TrendingTag = ({ title }: TrendingTagProps) => {
+  return (
+    <Link
+      className="text-custom-gray flex w-fit flex-row items-center gap-2 rounded-xl border border-gray-200 px-2 py-1 text-sm duration-300 hover:border-blue-500 hover:bg-blue-500 hover:text-white"
+      href="/tags/TODO"
+    >
+      {title}
+    </Link>
+  );
+};
--- src/shared/ui/tag/index.ts
+++ src/shared/ui/tag/index.ts
@@ -1,1 +1,2 @@
 export { Tag } from "./Tag";
+export { TrendingTag } from "./TrendingTag";
