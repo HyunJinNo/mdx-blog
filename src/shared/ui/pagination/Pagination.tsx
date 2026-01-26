19ab1ad0c7c2206fba6171395c2c0d2ee126404e
HyunJinNo <nhj9831@naver.com>
2026-01-26 16:07:00 +0900

    feat: 페이지네이션 기능 구현

--- src/shared/ui/pagination/Pagination.tsx
+++ src/shared/ui/pagination/Pagination.tsx
@@ -8,11 +8,10 @@ interface PaginationProps {
 }
 
 export const Pagination = ({ totalPages, currentPage }: PaginationProps) => {
-  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
-
   return (
     <nav className="flex h-7 items-center justify-center text-[#555555]">
       <ul className="flex flex-row items-center gap-3">
+        {/* 이전 페이지 */}
         <li>
           {currentPage === 1 ? (
             <div className="flex h-7 w-7 cursor-not-allowed items-center justify-center">
@@ -28,6 +27,47 @@ export const Pagination = ({ totalPages, currentPage }: PaginationProps) => {
           )}
         </li>
 
+        {/* 처음 페이지 번호 ~ 현재 페이지 번호 - 1 */}
+        {currentPage <= 3 ? (
+          Array.from({ length: currentPage - 1 }, (_, index) => index + 1).map(
+            (page) => (
+              <li key={page}>
+                <Link
+                  className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                  href={page === 1 ? "/" : `/page${page}`}
+                >
+                  {page}
+                </Link>
+              </li>
+            ),
+          )
+        ) : (
+          <>
+            <li>
+              <Link
+                className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                href="/"
+              >
+                1
+              </Link>
+            </li>
+            <li>
+              <span className="text-custom-gray/50 cursor-not-allowed">
+                ...
+              </span>
+            </li>
+            <li>
+              <Link
+                className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                href={`page${currentPage - 1}`}
+              >
+                {currentPage - 1}
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* 현재 페이지 번호 */}
         <li>
           <Link
             className="flex h-7 items-center justify-center rounded-lg bg-gray-100 px-2"
@@ -37,22 +77,48 @@ export const Pagination = ({ totalPages, currentPage }: PaginationProps) => {
           </Link>
         </li>
 
-        {/* {pages.map((page) => (
-          <li key={page}>
-            <Link
-              className={[
-                page === currentPage
-                  ? "bg-gray-100"
-                  : "outline-gray-200 duration-150 hover:outline",
-                "flex h-7 items-center justify-center rounded-lg px-2",
-              ].join(" ")}
-              href={page === 1 ? "/" : `/page${page}`}
-            >
-              {page}
-            </Link>
-          </li>
-        ))} */}
+        {/* 현재 페이지 번호 + 1 ~ 마지막 페이지 번호 */}
+        {currentPage + 2 >= totalPages ? (
+          Array.from(
+            { length: totalPages - currentPage },
+            (_, index) => index + currentPage + 1,
+          ).map((page) => (
+            <li key={page}>
+              <Link
+                className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                href={`/page${page}`}
+              >
+                {page}
+              </Link>
+            </li>
+          ))
+        ) : (
+          <>
+            <li>
+              <Link
+                className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                href={`/page${currentPage + 1}`}
+              >
+                {currentPage + 1}
+              </Link>
+            </li>
+            <li>
+              <span className="text-custom-gray/50 cursor-not-allowed">
+                ...
+              </span>
+            </li>
+            <li>
+              <Link
+                className="flex h-7 items-center justify-center rounded-lg px-1.5 outline-gray-200 duration-150 hover:outline"
+                href={`page${totalPages}`}
+              >
+                {totalPages}
+              </Link>
+            </li>
+          </>
+        )}
 
+        {/* 다음 페이지 */}
         <li>
           {currentPage === totalPages ? (
             <div className="flex h-7 w-7 cursor-not-allowed items-center justify-center">
