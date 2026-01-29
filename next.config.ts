28a9d135412994bb27601fcf39bf313ba88ebd58
HyunJinNo <nhj9831@naver.com>
2026-01-29 13:51:44 +0900

    chore: MDX 사용 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+});
 
 export default nextConfig;
