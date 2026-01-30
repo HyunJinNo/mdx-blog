e708986bef5d4e32788d53bf3ac6cfaa554bec2e
HyunJinNo <nhj9831@naver.com>
2026-01-30 20:31:24 +0900

    feat: MDX 파일의 metadata 가져오기 기능 구현

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -13,4 +13,20 @@ export function generateStaticParams() {
   return [{ post: "2025-03-12-fsd" }];
 }
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ post: string }>;
+}) {
+  const { post } = await params;
+  const mod = await import(`@/content/${post}.mdx`);
+  const metadata = mod.metadata;
+
+  console.log(metadata);
+
+  return {
+    author: metadata.author,
+  };
+}
+
 export const dynamicParams = false;
