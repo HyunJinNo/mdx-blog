3c62607594931957e0e4eb5bae727d590a66a57d
HyunJinNo <nhj9831@naver.com>
2026-01-30 21:18:14 +0900

    feat: Frontmatter 표시 기능 구현

--- src/app/posts/[post]/layout.tsx
+++ src/app/posts/[post]/layout.tsx
@@ -15,19 +15,24 @@ export default async function MdxLayout({
   // Create any shared layout or styles here
   return (
     <main className="flex w-full flex-col">
-      <header>
+      <header className="flex flex-col">
         <h1 className="mt-12 mb-2 text-3xl font-semibold text-black">
           {metadata.title}
         </h1>
         <p className="mb-6 text-lg">{metadata.description}</p>
-        <div>
+        <div className="text-custom-gray flex flex-col gap-4 text-sm">
           <span>
             Posted: <time>{metadata.date}</time>
           </span>
-          <div className="relative">
-            <Image src={metadata.imagePath} alt="Preview Image" fill={true} />
+          <div className="relative aspect-video">
+            <Image
+              className="rounded-xl"
+              src={metadata.imagePath}
+              alt="Preview Image"
+              fill={true}
+            />
           </div>
-          <div>
+          <div className="flex flex-row items-center justify-between">
             <span>By HyunJinNo</span>
             <span>10 min read</span>
           </div>
