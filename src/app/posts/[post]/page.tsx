72c913ff86e6dddb0930c08f3a5451eecf4f246f
HyunJinNo <nhj9831@naver.com>
2026-01-30 10:32:23 +0900

    feat: MDX 파일 동적 임포트 적용

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -4,13 +4,13 @@ export default async function Page({
   params: Promise<{ post: string }>;
 }) {
   const { post } = await params;
-  // const { default: Post } = await import(`@/content/${post}.mdx`);
+  const { default: Post } = await import(`@/content/${post}.mdx`);
 
-  // return <Post />;
+  return <Post />;
 }
 
 export function generateStaticParams() {
-  return [{ post: "welcome" }, { post: "about" }];
+  return [{ post: "2025-03-12-fsd" }];
 }
 
 export const dynamicParams = false;
