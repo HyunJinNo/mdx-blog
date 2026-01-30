18de4da9d673401022af6a4900a36c2cce654993
HyunJinNo <nhj9831@naver.com>
2026-01-30 21:12:18 +0900

    feat: title, description 표시 기능 구현

--- src/app/posts/[post]/layout.tsx
+++ src/app/posts/[post]/layout.tsx
@@ -1,4 +1,39 @@
-export default function MdxLayout({ children }: { children: React.ReactNode }) {
+import { PostMetadata } from "@/shared/model";
+import Image from "next/image";
+
+export default async function MdxLayout({
+  params,
+  children,
+}: {
+  params: Promise<{ post: string }>;
+  children: React.ReactNode;
+}) {
+  const { post } = await params;
+  const mod = await import(`@/content/${post}.mdx`);
+  const metadata: PostMetadata = mod.metadata;
+
   // Create any shared layout or styles here
-  return <main className="flex w-full flex-col">{children}</main>;
+  return (
+    <main className="flex w-full flex-col">
+      <header>
+        <h1 className="mt-12 mb-2 text-3xl font-semibold text-black">
+          {metadata.title}
+        </h1>
+        <p className="mb-6 text-lg">{metadata.description}</p>
+        <div>
+          <span>
+            Posted: <time>{metadata.date}</time>
+          </span>
+          <div className="relative">
+            <Image src={metadata.imagePath} alt="Preview Image" fill={true} />
+          </div>
+          <div>
+            <span>By HyunJinNo</span>
+            <span>10 min read</span>
+          </div>
+        </div>
+      </header>
+      {children}
+    </main>
+  );
 }
--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -13,20 +13,4 @@ export function generateStaticParams() {
   return [{ post: "2025-03-12-fsd" }];
 }
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ post: string }>;
-}) {
-  const { post } = await params;
-  const mod = await import(`@/content/${post}.mdx`);
-  const metadata = mod.metadata;
-
-  console.log(metadata); // TODO
-
-  return {
-    author: metadata.author,
-  };
-}
-
 export const dynamicParams = false;
