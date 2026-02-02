38d4b88086762dc2f180596fd982f2c2248f86a5
HyunJinNo <nhj9831@naver.com>
2026-02-02 16:09:06 +0900

    docs: 2023-10-11-abstract-factory 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -13,6 +13,7 @@ export function generateStaticParams(): { post: string }[] {
   return [
     { post: "2022-06-03-dgt-bluetooth-e-board" },
     { post: "2022-12-24-dgt-pi-chess-computer" },
+    { post: "2023-10-11-abstract-factory" },
     { post: "2025-03-12-fsd" },
   ];
 }
