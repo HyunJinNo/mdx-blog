349149c06837224d96f61f5e61f4ea1f6383433e
HyunJinNo <nhj9831@naver.com>
2026-02-03 16:10:56 +0900

    docs: 2024-06-06-ufw 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -48,6 +48,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-05-21-segment-tree" },
     { post: "2024-06-05-commands" },
     { post: "2024-06-05-raspberry-pi-setting" },
+    { post: "2024-06-06-ufw" },
     { post: "2025-03-12-fsd" },
   ];
 }
