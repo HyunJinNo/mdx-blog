00b9f3e8615a2b64bf61e1b0d972a1e7f009365b
HyunJinNo <nhj9831@naver.com>
2026-02-04 19:28:29 +0900

    docs: 2024-07-16-challenge-day2 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -62,6 +62,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-07-07-react-quill" },
     { post: "2024-07-10-nestjs-typeorm" },
     { post: "2024-07-15-challenge-day1" },
+    { post: "2024-07-16-challenge-day2" },
     { post: "2025-03-12-fsd" },
   ];
 }
