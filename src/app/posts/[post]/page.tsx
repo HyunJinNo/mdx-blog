48677a32425c8ca7a7e9828568f0d67e22073235
HyunJinNo <nhj9831@naver.com>
2026-02-05 19:40:20 +0900

    docs: 2024-08-10-nestjs-pipe 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -78,6 +78,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-08-05-challenge-day16" },
     { post: "2024-08-07-challenge-day18" },
     { post: "2024-08-09-regular-expression" },
+    { post: "2024-08-10-nestjs-pipe" },
     { post: "2025-03-12-fsd" },
   ];
 }
