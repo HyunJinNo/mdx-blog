c406522e7de58d86975a7c2bb453bbea9139a932
HyunJinNo <nhj9831@naver.com>
2026-02-07 21:05:06 +0900

    docs: 2024-09-30-virtual-dom 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -94,6 +94,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-09-24-prettier-eslint" },
     { post: "2024-09-28-babel" },
     { post: "2024-09-29-webpack" },
+    { post: "2024-09-30-virtual-dom" },
     { post: "2025-03-12-fsd" },
   ];
 }
