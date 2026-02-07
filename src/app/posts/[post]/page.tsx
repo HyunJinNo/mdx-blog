08bca77c0722ec514714d42d38438f9f8c3a71e0
HyunJinNo <nhj9831@naver.com>
2026-02-07 20:41:13 +0900

    docs: 2024-09-28-babel 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -92,6 +92,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-09-18-forwardref" },
     { post: "2024-09-23-jsx" },
     { post: "2024-09-24-prettier-eslint" },
+    { post: "2024-09-28-babel" },
     { post: "2025-03-12-fsd" },
   ];
 }
