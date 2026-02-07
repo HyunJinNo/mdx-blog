897d203845bd649ca24bad59de3cfa57e1a5550b
HyunJinNo <nhj9831@naver.com>
2026-02-07 22:58:25 +0900

    docs: 2024-11-03-github-actions-ci-cd 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -100,6 +100,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2024-10-20-puppeteer" },
     { post: "2024-10-21-expressjs-socket" },
     { post: "2024-10-23-notion-team-project-template" },
+    { post: "2024-11-03-github-actions-ci-cd" },
     { post: "2025-03-12-fsd" },
   ];
 }
