90a28b7278fa8e243ff086d3209b8a05dde443a7
HyunJinNo <nhj9831@naver.com>
2026-02-09 16:03:11 +0900

    docs: 2025-02-06-execution-context 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -111,6 +111,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2025-01-30-ollama" },
     { post: "2025-02-04-xml-json" },
     { post: "2025-02-05-hoisting" },
+    { post: "2025-02-06-execution-context" },
     { post: "2025-03-12-fsd" },
   ];
 }
