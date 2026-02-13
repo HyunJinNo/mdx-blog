5dc006e8fc6e8b30eee684dba3357cb9a3417d80
HyunJinNo <nhj9831@naver.com>
2026-02-13 15:31:56 +0900

    docs: 2025-05-29-react-navigation 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -133,6 +133,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2025-04-14-origin-and-site" },
     { post: "2025-04-27-nextjs-docker" },
     { post: "2025-05-29-react-native-dotenv" },
+    { post: "2025-05-29-react-navigation" },
   ];
 }
 
