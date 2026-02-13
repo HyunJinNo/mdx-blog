e0e97492b7a6449114b4058844fdbd1115872fe1
HyunJinNo <nhj9831@naver.com>
2026-02-13 16:57:49 +0900

    docs: 2025-11-19-localtunnel 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -141,6 +141,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2025-08-03-eclipse-lombok" },
     { post: "2025-10-01-lis" },
     { post: "2025-10-13-topological-sorting" },
+    { post: "2025-11-19-localtunnel" },
   ];
 }
 
