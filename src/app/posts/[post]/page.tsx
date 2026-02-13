8a13e794235c66b03b9ad47c943eb61e66011cd1
HyunJinNo <nhj9831@naver.com>
2026-02-13 15:20:32 +0900

    docs: 2025-04-27-nextjs-docker 문서 추가

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -131,6 +131,7 @@ export function generateStaticParams(): { post: string }[] {
     { post: "2025-04-11-sitemap-robotstxt" },
     { post: "2025-04-13-cookie" },
     { post: "2025-04-14-origin-and-site" },
+    { post: "2025-04-27-nextjs-docker" },
   ];
 }
 
