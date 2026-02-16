89caad95fee835afb81386dd871fc5b888b855ab
HyunJinNo <nhj9831@naver.com>
2026-02-16 15:50:08 +0900

    design: PostCard UI 수정

--- src/shared/ui/card/PostCard.tsx
+++ src/shared/ui/card/PostCard.tsx
@@ -27,9 +27,11 @@ export const PostCard = ({
         href={`/posts/${postPath}`}
       >
         <div className="absolute top-0 right-0 bottom-0 left-0 z-10 rounded-md duration-300 hover:bg-gray-200/30" />
-        <div className="flex flex-col pt-7 pr-7 pb-5 pl-7">
-          <h1 className="text-xl font-bold">{title}</h1>
-          <p className="text-custom-gray mt-2">{description}</p>
+        <div className="flex flex-col justify-between pt-7 pr-7 pb-5 pl-7">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-xl font-bold">{title}</h1>
+            <p className="text-custom-gray line-clamp-2">{description}</p>
+          </div>
           <div className="text-custom-gray mt-4 flex flex-row items-center gap-7">
             <div className="flex flex-row items-center gap-2">
               <FaRegCalendar className="text-sm" />
