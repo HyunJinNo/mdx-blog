b8c80550365cb65db17243d4b9c5cba079818845
HyunJinNo <nhj9831@naver.com>
2026-02-16 15:35:51 +0900

    refactor: PostCard 컴포넌트 리팩토링

--- src/shared/ui/card/PostCard.tsx
+++ src/shared/ui/card/PostCard.tsx
@@ -3,37 +3,48 @@ import Link from "next/link";
 import { FaRegCalendar } from "@react-icons/all-files/fa/FaRegCalendar";
 import { FaFolderOpen } from "@react-icons/all-files/fa/FaFolderOpen";
 
-export const PostCard = () => {
+interface PostCardProps {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  imagePath: string;
+  postPath: string;
+}
+
+export const PostCard = ({
+  title,
+  description,
+  date,
+  category,
+  imagePath,
+  postPath,
+}: PostCardProps) => {
   return (
     <article>
       <Link
         className="group relative flex h-45 flex-row justify-between rounded-md border border-gray-200 shadow"
-        href="/posts/TODO"
+        href={`/posts/${postPath}`}
       >
         <div className="absolute top-0 right-0 bottom-0 left-0 z-10 rounded-md duration-300 hover:bg-gray-200/30" />
         <div className="flex flex-col pt-7 pr-7 pb-5 pl-7">
-          <h1 className="text-xl font-bold">
-            중간에서 만나기 (Meet in the Middle) 알고리즘
-          </h1>
-          <p className="text-custom-gray mt-2">
-            중간에서 만나기 (Meet in the Middle) 알고리즘에 대해 정리한
-            페이지입니다.
-          </p>
+          <h1 className="text-xl font-bold">{title}</h1>
+          <p className="text-custom-gray mt-2">{description}</p>
           <div className="text-custom-gray mt-4 flex flex-row items-center gap-7">
             <div className="flex flex-row items-center gap-2">
               <FaRegCalendar className="text-sm" />
-              <time className="text-sm">Jan 19, 2026</time>
+              <time className="text-sm">{date}</time>
             </div>
             <div className="flex flex-row items-center gap-2">
               <FaFolderOpen />
-              <span className="text-sm">Algorithms</span>
+              <span className="text-sm">{category}</span>
             </div>
           </div>
         </div>
-        <div className="relative h-45 w-80">
+        <div className="relative h-45 min-w-80">
           <Image
             className="rounded-r-md"
-            src="/images/background.avif"
+            src={imagePath}
             alt="preview Image"
             fill={true}
           />
