3c5905debcda6b835042c866b4b2ebec56a0242e
HyunJinNo <nhj9831@naver.com>
2026-02-19 20:08:06 +0900

    feat: 카테고리별 블로그 게시글 목록 불러오기 기능 구현

--- src/entities/post/index.ts
+++ src/entities/post/index.ts
@@ -1,6 +1,7 @@
 export { getArchiveList } from "./model/getArchiveList";
 export { getCategoryList } from "./model/getCategoryList";
 export { getPostList } from "./model/getPostList";
+export { getPostListByCategory } from "./model/getPostListByCategory";
 export { getRecentlyUpdatedPostList } from "./model/getRecentlyUpdatedPostList";
 export { getTrendingTagList } from "./model/getTrendingTagList";
 export { type PostMetadata } from "./model/postMetadata";
