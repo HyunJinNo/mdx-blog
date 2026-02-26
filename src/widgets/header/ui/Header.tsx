8bafcc94356f39b7403994871b40c46078a3c885
HyunJinNo <nhj9831@naver.com>
2026-02-26 16:49:46 +0900

    design: Header 반응형 디자인 적용

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,12 +1,27 @@
+"use client";
+
+import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
+import { FaBars } from "@react-icons/all-files/fa/FaBars";
 import { SearchInput } from "@/features/searchPostListByTitle";
 import { Breadcrumb } from "@/shared/ui/breadcrumb";
-import { FaSearch } from "@react-icons/all-files/fa/FaSearch";
+import { useHeaderTitle } from "../model/useHeaderTitle";
 
 export const Header = () => {
+  const { headerTitle } = useHeaderTitle();
+
   return (
-    <header className="flex h-12 w-full flex-row items-center justify-between px-6">
+    <header className="desktop:pr-20 flex h-12 w-full flex-row items-center justify-between">
       <Breadcrumb />
-      <div className="relative w-50">
+      <button className="laptop:hidden">
+        <FaBars className="text-custom-gray" />
+      </button>
+      <div className="laptop:hidden text-custom-gray text-lg font-medium">
+        {headerTitle}
+      </div>
+      <button className="laptop:hidden">
+        <FaSearch className="text-custom-gray" />
+      </button>
+      <div className="laptop:block relative hidden w-50">
         <FaSearch className="text-custom-gray/50 absolute top-2 left-3 scale-90" />
         <SearchInput />
       </div>
